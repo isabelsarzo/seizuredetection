9a14314240f670ae4650f0c8e7b3914dce783a1d
isabelsarzo <isabel.sarzo@gmail.com>
2024-07-20 18:53:40 -0400

    implemented notch filter

--- preprocessing.py
+++ preprocessing.py
@@ -4,34 +4,38 @@ from plot_tools import plotEMG, plotFreq
 from scipy.signal import butter, sosfilt
 
 def highpass(data, cutoff):
-    # TODO: write the docstring to explain function
-    filtdesign = butter(2, cutoff, 'highpass', fs=1000, output='sos')
-    data_hp = sosfilt(filtdesign, data)
+    # TODO: write the docstring 
+    hpass_design = butter(2, cutoff, 'highpass', fs=1000, output='sos')
+    data_hp = sosfilt(hpass_design, data)
     return data_hp
 
 def notch(data, cutoffs):
-    # TODO
-    return None
+    # TODO: write the docstring
+    notch_design = butter(2, cutoffs, 'bandstop', fs=1000, output='sos')
+    data_notch = sosfilt(notch_design, data)
+    return data_notch
 
-def preprocess(data, hp_cutoff):
-    # TODO: write the docstring to explain function
+def preprocess(data, hp_cutoff, notch_cutoffs):
+    # TODO: write the docstring
     time = data.index
     channels = data.columns
     
     # High-pass filter
     print("FILTERING - HIGHPASS")
     data_hp = data.apply(lambda x: highpass(x, hp_cutoff), axis=0)
+    print(f"Type of data after highpass: {type(data_hp)}")
 
     # Notch filter
-    # ... TODO: design and apply bandstop filter
+    print("FILTERING - BANDSTOP")
+    data_filtered = data_hp.apply(lambda x: notch(x, notch_cutoffs), axis=0)
 
     # Recreate dataframe
-    data_f = pd.DataFrame(data_hp, index=time, columns=channels)
+    data_f = pd.DataFrame(data_filtered, index=time, columns=channels)
 
     return data_f
 
 data, time_s = load_data(308, 20240703, 'A', 44, 5, 'temp', 'emg')
-data_f = preprocess(data, 100)
+data_f = preprocess(data, 10, [59, 61])
 
 plotEMG(data, 'all', 3500, 0, "Original")
 signal = data.iloc[:, 1].values
