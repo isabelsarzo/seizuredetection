f1c00f60711d8f9bb51afd4276c166ed5134ecf5
isabelsarzo <isabel.sarzo@gmail.com>
2024-07-22 16:14:43 -0400

    created lowpass and bandpass functions, and also added docstrings

--- preprocessing.py
+++ preprocessing.py
@@ -1,58 +1,117 @@
-import pandas as pd
-from load_data import load_data
-from plot_tools import plotEMG, plotFreq
 from scipy.signal import butter, sosfilt
 
 def timerange(data, startTime, endTime):
+    """
+    Extracts a subset of the dataframe containing rows within a specified time interval.
+
+    Args:
+    -----------------------------------------------------------------
+    -data: dataframe from which the specified time interval will be extracted. 
+    -startTime: str, start timestamp of the time interval in the format 'yyyy-mm-dd HH:MM:SS'.
+    -endTime: str, end timestamp of the time interval in the format 'yyyy-mm-dd HH:MM:SS'.
+
+    Returns:
+    -----------------------------------------------------------------
+    -datarange: new dataframe containing only the rows within the specified time interval.
+
+    """
     datarange = data[startTime : endTime]
     return datarange
 
+def lowpass(data, cutoff):
+    """
+    Designs and applies a digital 2nd order lowpass butterworth filter.
+
+    Args:
+    -----------------------------------------------------------------
+    -data: dataframe containing the data to be filtered.
+    -cutoff: int, cutoff frequency (Hz) at which the gain drops -3dB.
+
+    Returns:
+    -----------------------------------------------------------------
+    -data_lp: dataframe containing the filtered data. 
+
+    """
+    lpass_design = butter(2, cutoff, 'lowpass', fs=1000, output='sos')
+    data_lp = sosfilt(lpass_design, data)
+    return data_lp
+
 def highpass(data, cutoff):
-    # TODO: write the docstring 
+    """
+    Designs and applies a digital 2nd order highpass butterworth filter.
+
+    Args:
+    -----------------------------------------------------------------
+    -data: dataframe containing the data to be filtered.
+    -cutoff: int, cutoff frequency (Hz) at which the gain drops -3dB.
+
+    Returns:
+    -----------------------------------------------------------------
+    -data_hp: dataframe containing the filtered data. 
+
+    """
     hpass_design = butter(2, cutoff, 'highpass', fs=1000, output='sos')
     data_hp = sosfilt(hpass_design, data)
     return data_hp
 
+def bandpass(data, cutoffs):
+    """
+    Designs and applies a digital 2nd order bandpass butterworth filter.
+
+    Args:
+    -----------------------------------------------------------------
+    -data: dataframe containing the data to be filtered.
+    -cutoffs: length-2 sequence, cutoff frequencies (Hz) at which the gain drops -3dB.
+
+    Returns:
+    -----------------------------------------------------------------
+    -data_bp: dataframe containing the filtered data. 
+
+    """
+    bpass_design = butter(2, cutoffs, 'bandpass', fs=1000, output='sos')
+    data_bp = sosfilt(bpass_design, data)
+    return data_bp
+
 def notch(data, cutoffs):
-    # TODO: write the docstring
+    """
+    Designs and applies a digital 2nd order bandstop (notch) butterworth filter.
+
+    Args:
+    -----------------------------------------------------------------
+    -data: dataframe containing the data to be filtered.
+    -cutoffs: length-2 sequence, cutoff frequencies (Hz) at which the gain drops -3dB.
+
+    Returns:
+    -----------------------------------------------------------------
+    -data_notch: dataframe containing the filtered data. 
+
+    """
     notch_design = butter(2, cutoffs, 'bandstop', fs=1000, output='sos')
     data_notch = sosfilt(notch_design, data)
     return data_notch
 
 def preprocess(data, hp_cutoff, notch_cutoffs):
-    # TODO: write the docstring
-    time = data.index
-    channels = data.columns
-    
+    """
+    Applies the established pre-processing pipeline: 
+        1. Highpass filter
+        2. Notch filter
+
+    Args:
+    -----------------------------------------------------------------
+    -data: dataframe containing the data to be filtered.
+    -hp_cutoff: int, cutoff frequency (Hz) for the highpass filter.
+    -notch_cutoffs: length-2 sequence, cutoff frequencies (Hz) for bandstop filter.
+
+    Returns:
+    -----------------------------------------------------------------
+    -data_filtered: dataframe containing the filtered data. 
+
+    """ 
     # High-pass filter
     print("FILTERING - HIGHPASS")
     data_hp = data.apply(lambda x: highpass(x, hp_cutoff), axis=0)
-    print(f"Type of data after highpass: {type(data_hp)}")
 
     # Notch filter
     print("FILTERING - BANDSTOP")
     data_filtered = data_hp.apply(lambda x: notch(x, notch_cutoffs), axis=0)
-
-    # Recreate dataframe
-    data_f = pd.DataFrame(data_filtered, index=time, columns=channels)
-    data_f.index = pd.to_datetime(data_f.index, format='%d-%b-%Y %H:%M:%S.%f')
-
-    return data_f
-
-data, time_s = load_data(308, 20240703, 'A', 44, 5, 'temp', 'emg')
-datarange = timerange(data, '2024-07-03 19:54:19', '2024-07-03 19:56:21')
-plotEMG(datarange, 'all', 3500, 1, 'seizure')
-
-# data_f = preprocess(data, 10, [59, 61])
-
-# plotEMG(data, 'all', 3500, 0, "Original")
-# signal = data.iloc[:, 1].values
-# channel = "Original Left Trapeze"
-# plotFreq(signal, channel, "FreqOriginal")
-
-# plotEMG(data_f, 'all', 3500, 0, "Filtered")
-# signal_hp = data_f.iloc[:, 1].values
-# channel2 = "Filtered Left Trapeze"
-# plotFreq(signal_hp, channel2, "FreqFiltered")
-
-# TODO: create a function to extract a timerange of data
+    return data_filtered
